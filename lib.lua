b98e398dbe10cf4b1a531ca065e040d9fba93250
Enmiori <36536701+Enmiori@users.noreply.github.com>
2021-10-01 22:11:07 +0700

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1713,7 +1713,7 @@ end)
 inputService.InputChanged:connect(function(input)
 	if input.UserInputType == Enum.UserInputType.MouseMovement and library.cursor then
 		local mouse = inputService:GetMouseLocation() + Vector2.new(0, -36)
-		library.cursor.Position = UDim2.new(0, mouse.X - 10000, 0, mouse.Y - 10000)
+		library.cursor.Position = UDim2.new(0, mouse.X - 2000, 0, mouse.Y - 2000)
 	end
 	if input == dragInput and dragging then
 		update(input)
